0a2dba8c863f7fc00764d9bbffec29810e26764e
ej-rarus <lpl2001@naver.com>
2022-09-06 15:37:11 +0900

    custom component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import './App.css';
 import { data } from './data';
-
+import { Details } from './custom';
 
 import 라쿤 from './img/raccoon.jpeg';
  
@@ -83,25 +83,7 @@ function Merchandize(props) {
   );
 }
 
-function Details(props) {
-  return(
-    <div className="container">
-      <div className="row">
-        
-        <div className="col-md-6">
-        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-        </div>
-        
-        <div className="col-md-6">
-        <h4 className="pt-5">상품명</h4>
-        <p>상품설명</p>
-        <p>120000원</p>
-        <button className="btn btn-danger">주문하기</button> 
-        </div>
-      </div>
-    </div> 
-  );
-}
+
 
 
 export default App;
