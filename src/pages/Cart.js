6b7cc2cfc37547c54a926dd305eca2afef574084
Eunjae TonyLee <lpl2001@naver.com>
2022-10-26 16:13:09 +0900

    commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,11 +7,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeName, changeAge } from "./../store/userSlice.js";
 import { addQty, decQty } from "./../store/cartSlice.js";
 
-function Child() {
+let Child = memo( function(){
   return (
     <div>Child임</div>
   )
-}
+})
 
 
 function Cart() {
@@ -21,7 +21,7 @@ function Cart() {
 
   return (
     <div>
-      <Child></Child>
+      <button onClick={()=>{setCount(count + 1)}}>+_</button>
       <h3>{state.user.age}살 {state.user.name}의 장바구니</h3>
       <button onClick={() => { dispatch(changeAge(100)) }}>버튼</button>
       <Table>
